019729d6f8dad35084aebbbea1e87a79a78f7103
twopyramids <sunburnmanager@gmail.com>
2025-11-06 04:12:03 +0000

    fix: pixel bars on odd-sized screens

--- src/lib/components/homepage/transitions/AccentToBase100.svelte
+++ src/lib/components/homepage/transitions/AccentToBase100.svelte
@@ -1,41 +1,43 @@
-<svg
-	width="100"
-	height="20"
-	viewBox="0 0 100 20"
-	version="1.1"
-	id="svg1"
-	xmlns:xlink="http://www.w3.org/1999/xlink"
-	xmlns="http://www.w3.org/2000/svg"
-	data-theme="sunburn-day"
-	class="aspect-10/2 size-full"
->
-	<defs id="defs1">
-		<linearGradient id="accentToBase100Gradient1">
-			<stop
-				style="stop-color:oklch(0.8201 0.1343 66.99);stop-opacity:1;"
-				offset="0"
-				id="accentToBase100Stop1"
+<div class="relative aspect-10/2 size-full" data-theme="sunburn-day">
+	<svg
+		width="100"
+		height="20"
+		viewBox="0 0 100 20"
+		version="1.1"
+		id="svg1"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		xmlns="http://www.w3.org/2000/svg"
+		class="size-full"
+	>
+		<defs id="defs1">
+			<linearGradient id="accentToBase100Gradient1">
+				<stop
+					style="stop-color:oklch(0.8201 0.1343 66.99);stop-opacity:1;"
+					offset="0"
+					id="accentToBase100Stop1"
+				/>
+				<stop
+					style="stop-color:oklch(0.5665 0.1156 79.45);stop-opacity:1;"
+					offset="1"
+					id="accentToBase100Stop2"
+				/>
+			</linearGradient>
+			<linearGradient
+				xlink:href="#accentToBase100Gradient1"
+				id="accentToBase100Gradient2"
+				x1="68.965446"
+				y1="1.6896036"
+				x2="68.965446"
+				y2="15.371583"
+				gradientUnits="userSpaceOnUse"
+				gradientTransform="scale(1,2)"
 			/>
-			<stop
-				style="stop-color:oklch(0.5665 0.1156 79.45);stop-opacity:1;"
-				offset="1"
-				id="accentToBase100Stop2"
-			/>
-		</linearGradient>
-		<linearGradient
-			xlink:href="#accentToBase100Gradient1"
-			id="accentToBase100Gradient2"
-			x1="68.965446"
-			y1="1.6896036"
-			x2="68.965446"
-			y2="15.371583"
-			gradientUnits="userSpaceOnUse"
-			gradientTransform="scale(1,2)"
+		</defs>
+		<rect style="fill:url(#accentToBase100Gradient2);" class="size-full" x="0" y="0" />
+		<path
+			class="fill-base-100"
+			d="m 0,11.253739 c 0,0 12.15314201,4.130016 21.38128801,4.466639 8.295236,0.302593 35.868343,-3.96698 43.559664,-3.344669 8.227152,0.665665 16.855642,3.14131 24.57117,3.14131 C 97.403257,15.517019 100,13 100,13 v 7 H 0 Z"
 		/>
-	</defs>
-	<rect style="fill:url(#accentToBase100Gradient2);" class="size-full" x="0" y="0" />
-	<path
-		class="fill-base-100"
-		d="m 0,11.253739 c 0,0 12.15314201,4.130016 21.38128801,4.466639 8.295236,0.302593 35.868343,-3.96698 43.559664,-3.344669 8.227152,0.665665 16.855642,3.14131 24.57117,3.14131 C 97.403257,15.517019 100,13 100,13 v 7 H 0 Z"
-	/>
-</svg>
+	</svg>
+	<div class="absolute bottom-0 z-10 h-2 w-full translate-y-1/2 bg-base-100"></div>
+</div>
--- src/lib/components/homepage/transitions/BaseContentToAccent.svelte
+++ src/lib/components/homepage/transitions/BaseContentToAccent.svelte
@@ -1,41 +1,43 @@
-<svg
-	width="100"
-	height="20"
-	viewBox="0 0 100 20"
-	version="1.1"
-	id="svg1"
-	xmlns:xlink="http://www.w3.org/1999/xlink"
-	xmlns="http://www.w3.org/2000/svg"
-	data-theme="sunburn-day"
-	class="aspect-10/2 size-full"
->
-	<defs id="defs1">
-		<linearGradient id="baseContentToAccentGradient1">
-			<stop
-				style="stop-color:oklch(0.3079 0.1097 28.08);stop-opacity:1;"
-				offset="0"
-				id="baseContentToAccentStop1"
+<div class="relative aspect-10/2 size-full" data-theme="sunburn-day">
+	<svg
+		width="100"
+		height="20"
+		viewBox="0 0 100 20"
+		version="1.1"
+		id="svg1"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		xmlns="http://www.w3.org/2000/svg"
+		class="size-full"
+	>
+		<defs id="defs1">
+			<linearGradient id="baseContentToAccentGradient1">
+				<stop
+					style="stop-color:oklch(0.3079 0.1097 28.08);stop-opacity:1;"
+					offset="0"
+					id="baseContentToAccentStop1"
+				/>
+				<stop
+					style="stop-color:oklch(0.5286 0.179591 36.3451);stop-opacity:1;"
+					offset="1"
+					id="baseContentToAccentStop2"
+				/>
+			</linearGradient>
+			<linearGradient
+				xlink:href="#baseContentToAccentGradient1"
+				id="baseContentToAccentGradient2"
+				x1="68.965446"
+				y1="1.6896036"
+				x2="68.965446"
+				y2="15.371583"
+				gradientUnits="userSpaceOnUse"
+				gradientTransform="scale(1,2)"
 			/>
-			<stop
-				style="stop-color:oklch(0.5286 0.179591 36.3451);stop-opacity:1;"
-				offset="1"
-				id="baseContentToAccentStop2"
-			/>
-		</linearGradient>
-		<linearGradient
-			xlink:href="#baseContentToAccentGradient1"
-			id="baseContentToAccentGradient2"
-			x1="68.965446"
-			y1="1.6896036"
-			x2="68.965446"
-			y2="15.371583"
-			gradientUnits="userSpaceOnUse"
-			gradientTransform="scale(1,2)"
+		</defs>
+		<rect style="fill:url(#baseContentToAccentGradient2);" class="size-full" x="0" y="0" />
+		<path
+			class="fill-accent"
+			d="m 0,15 c 0,0 6.7093566,-1.899123 15.937502,-1.5625 8.295236,0.302593 17.371179,2.940189 25.0625,3.5625 8.227152,0.665665 37.284472,-4 45,-4 C 93.715531,13 100,15 100,15 v 5 H 0 Z"
 		/>
-	</defs>
-	<rect style="fill:url(#baseContentToAccentGradient2);" class="size-full" x="0" y="0" />
-	<path
-		class="fill-accent"
-		d="m 0,15 c 0,0 6.7093566,-1.899123 15.937502,-1.5625 8.295236,0.302593 17.371179,2.940189 25.0625,3.5625 8.227152,0.665665 37.284472,-4 45,-4 C 93.715531,13 100,15 100,15 v 5 H 0 Z"
-	/>
-</svg>
+	</svg>
+	<div class="absolute bottom-0 z-10 h-2 w-full translate-y-1/2 bg-accent"></div>
+</div>
